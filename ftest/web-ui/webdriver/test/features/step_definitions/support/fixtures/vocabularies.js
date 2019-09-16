0e1b80a58001738d115ac878389d1cf29f387317
Guillaume Renard <grenard@nuxeo.com>
2019-09-16 08:34:37 +0200

    NXP-27831: fix sym link to vocabulary entry

--- ftest/web-ui/webdriver/test/features/step_definitions/support/fixtures/vocabularies.js
+++ ftest/web-ui/webdriver/test/features/step_definitions/support/fixtures/vocabularies.js
@@ -1,1 +1,27 @@
-/home/fox/10.10-workspace/mp/plugin-nuxeo-web-ui/nuxeo-web-ui-ftest/test/features/step_definitions/support/fixtures/vocabularies.js
+import {
+  After,
+} from 'cucumber';
+import nuxeo from '../services/client';
+
+global.addedVocabularyEntries = [];
+
+fixtures.vocabularies = {
+  createEntry: (directoryName, id, properties) => nuxeo.request(`/directory/${directoryName}/${id}`).get()
+    .catch(() => {
+      const params = {
+        body: {
+          'entity-type': 'directoryEntry',
+          directoryName,
+          properties,
+        },
+      };
+      return nuxeo.request(`/directory/${directoryName}`).post(params)
+        .then((response) => {
+          addedVocabularyEntries.push(response);
+        }).catch(() => {});
+    }),
+};
+After(() => Promise.all(addedVocabularyEntries
+  .map(entry => nuxeo.request(`/directory/${entry.directoryName}`).delete(entry)
+    .catch(() => {}))) // eslint-disable-line arrow-body-style
+  .then(() => { addedVocabularyEntries = []; }));
